ea8262a43ff7799127e9fc2e3a6f1d0bbbd50145
Piotr Kluziok <piotrkluziok@gmail.com>
2024-06-12 17:52:36 +0200

    Add apps for project

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movie_app',
+    'review_app',
+    'user_app',
 ]
 
 MIDDLEWARE = [
