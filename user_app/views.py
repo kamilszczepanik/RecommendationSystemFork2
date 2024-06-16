4c19fc76b12bc8b74e071271205483a16f81f26f
Piotr Kluziok <piotrkluziok@gmail.com>
2024-06-16 09:25:23 +0200

    List user on user_details page

--- user_app/views.py
+++ user_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Users, Favouritemovies
 
 
 # Create your views here.
 def display_user_page(request):
-    return render(request, 'user_details.html')
+    users = Users.get_users_head()
+    return render(request, 'user_details.html', {'users': users})
 
 
 def display_login_page(request):
