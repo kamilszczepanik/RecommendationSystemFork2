a07389a249a4f81a2fd4d5de260d7324e6c61ce5
Piotr Kluziok <piotrkluziok@gmail.com>
2024-06-26 10:46:35 +0200

    Add 3 most popular movies based on ratings and votes

--- movie_recommendation/views.py
+++ movie_recommendation/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from movie_app.models import Movies
 
 def homepage(request):
-    return render(request, 'index.html')
+    popular_movies = Movies.get_popular_movies()
+    context = {
+        'popular_movies': popular_movies
+    }
+    return render(request, 'index.html', context=context)
 
 
 def about(request):
