b0ab1aca2cc5d77aefad8365c45ac3397a6cc82e
Piotr Kluziok <piotrkluziok@gmail.com>
2024-09-11 19:59:57 +0200

    Remove debugging views for user

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,11 +4,8 @@ from . import views
 app_name = 'user_app'
 
 urlpatterns = [
-    path('', views.display_users_page, name='users'),
     path('details/<int:user_id>/', views.display_user_details_page, name='user_details'),
-    path('details/', views.display_sample_user_details_page, name='sample_user_details'),
     path('register/', views.display_register_page, name='register'),
     path('login/', views.display_login_page, name='login'),
     path('logout/', views.display_logout_page, name='logout'),
-
 ]
